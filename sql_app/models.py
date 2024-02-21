04b7f9be269a7bea83217f72ee05c4de7f2f522f
kizataka <chongxiongmu@gmail.com>
2024-02-22 08:49:26 +0900

    テーブルの追加

---
+++ sql_app/models.py
@@ -0,0 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from .database import Base
+
+# ユーザー情報
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
