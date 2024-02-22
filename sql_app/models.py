2579c8103ca116f0ff7488d57114c94f099bd7c7
kizataka <chongxiongmu@gmail.com>
2024-02-22 19:54:59 +0900

    カラム名を変更

--- sql_app/models.py
+++ sql_app/models.py
@@ -8,4 +8,4 @@ class User(Base):
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True)
     email = Column(String, unique=True, index=True)
-    password = Column(String)
+    hashed_password = Column(String)
