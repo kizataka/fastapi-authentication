f3810158b8d78a1914fd3a91b1b733a738805aec
kizataka <chongxiongmu@gmail.com>
2024-02-22 09:05:42 +0900

    emailカラムの追加

--- sql_app/models.py
+++ sql_app/models.py
@@ -7,4 +7,5 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, unique=True, index=True)
+    email = Column(String, unique=True, index=True)
     hashed_password = Column(String)
